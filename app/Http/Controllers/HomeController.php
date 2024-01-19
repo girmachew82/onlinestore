c9a2a66a15d71329e70f1629a36d9ab3b125b56c
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-01-19 10:20:24 +0300

    First code refactor Homecontroller about blade app blade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,13 +11,14 @@ return view('home.index')->with("viewData", $viewData);
 }
 public function about()
 {
-$data1 = "About us - Online Store";
-$data2 = "About us";
-$description = "This is an about page ...";
-$author = "Developed by: Your Name";
+    $viewData = [];
+    $viewData['title'] = "About us - Online Store";
+    $viewData['subtitle'] = "About us";
+    $ $viewData['description'] = "This is an about page ...";
+    $viewData['author'] = "Developed by: Your Name";
 return view('home.about')->with("title", $data1)
-->with("subtitle", $data2)
-->with("description", $description)
-->with("author", $author);
+        ->with("subtitle", $data2)
+        ->with("description", $description)
+        ->with("author", $author);
 }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark top-nav py-4">
         <div class="container">
-            <a class="navbar-brand" href="#">Online Store</a>
+            <a class="navbar-brand" href="{{ route('home.index') }}">Online Store</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -22,10 +22,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link" href="{{ route('home.about') }}">About</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
