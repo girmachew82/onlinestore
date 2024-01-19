5432e4322fa00efb73de43aec752bbf9a710933e
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-01-19 13:42:08 +0300

    List products in admin panel

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AdminHomeController extends Controller
 {
     public function index()
     {
         $viewData = [];
-        $viewData["title"] = "Admin Page - Admin - Online Store";
-        return view('admin.home.index')->with("viewData", $viewData);
+        $viewData["title"] = "Admin Page - Products - Online Store";
+        $viewData["products"] = Product::all();
+        return view('admin.product.index')->with("viewData", $viewData);
     }
 }
+
