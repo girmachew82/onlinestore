ed42744ef0bb73a902b4322c1ab000332f00e0f2
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-01-19 10:48:00 +0300

    Coding standard

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/', function () {
     return view('home.index') ->with('viewData',$viewData);
 });
 */
-Route::get('/','App\Http\Controllers\HomeController@index')->name('home.index');
-Route::get('/about','App\Http\Controllers\HomeController@about')->name('home.about');
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
