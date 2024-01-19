d53efc6682b432dec16315a5f284b63a05507634
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-01-19 11:04:09 +0300

    Products with Dummy Data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,9 @@
                         <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}">Home</a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link" href="{{ route('product.index') }}">Products</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="{{ route('home.about') }}">About</a>
                     </li>
                     <li class="nav-item dropdown">
