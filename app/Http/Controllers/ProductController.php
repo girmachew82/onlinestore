2fa2c3daaed49403051ae4960f01086931146af3
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-01-19 11:16:03 +0300

    Gettting Product from Database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,28 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    /*
     public static $products = [
         ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "image" => "game.jpeg", "price"=>"1000"],
         ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "image" => "safe.jpeg", "price"=>"999"],
         ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "image" => "submarine.jpeg", "price"=>"30"],
         ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "image" => "game.jpeg", "price"=>"100"]
     ];
+    */
     public function index()
     {
         $viewData = [];
         $viewData["title"] = "Products - Online Store";
         $viewData["subtitle"] = "List of products";
-        $viewData["products"] = ProductController::$products;
+        $viewData["products"] = Product::all();
         return view('product.index')->with("viewData", $viewData);
     }
     public function show($id)
     {
         $viewData = [];
-        $product = ProductController::$products[$id-1];
+        $product = Product::findOrFail();
         $viewData["title"] = $product["name"]." - Online Store";
         $viewData["subtitle"] = $product["name"]." - Product information";
         $viewData["product"] = $product;
